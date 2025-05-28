05b9846b0075a6fcff6c5e823a2213c7a60b5b38
Zoticisking <aortiz111@mymail.lausd.net>
2025-05-28 14:17:34 -0700

    test

---
+++ src/main/java/com/Zotic/Zotic/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args){
+        int[] scores = new int[20];
+        for (int i : scores) {
+            System.out.println(i);
+
+        }
+    }
+}
