43c1cddc1da613793f4b25ce551e8f1b923ce38c
Zoticisking <aortiz111@mymail.lausd.net>
2025-05-29 14:16:25 -0700

    Update Test.java

--- src/main/java/com/Zotic/Zotic/Test.java
+++ src/main/java/com/Zotic/Zotic/Test.java
@@ -1,9 +1,21 @@
-public class Test {
-    public static void main(String[] args){
-        int[] scores = new int[20];
-        for (int i : scores) {
-            System.out.println(i);
+package com.zotic.zotic;
 
+import com.birdbrain.Finch;
+
+public class FinchDemo {
+    public static void main(String[] args) {
+        Finch myFinch = new Finch();
+
+        myFinch.playNote(60,0.5);
+
+        for (int i = 0; i < 10; i++) {
+            myFinch.setBeak(100, 100, 100);
+            myFinch.pause(1);
+            myFinch.setBeak(0, 0, 0);
+            myFinch.pause(1);
         }
+
+        myFinch.stopAll();
+        myFinch.disconnect();
     }
 }
